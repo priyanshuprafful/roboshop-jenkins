b95830d9db4eb088a9c5f640e003447b05a026ff
priyanshuprafful <priyanshuprafful@gmail.com>
2024-05-15 14:42:46 +0530

    updating ./roboshop-jenkins

--- vars.tf
+++ vars.tf
@@ -5,12 +5,12 @@ variable "folders" {
 variable "jobs" {
   default = [
     { name = "roboshop" , folder = "Infra" , repo_url = "https://github.com/priyanshuprafful/roboshop-infra"},
-    { name = "frontend" , folder = "CI-Pipeline" , repo_url = "https://github.com/priyanshuprafful/roboshop-infra"},
-    { name = "catalogue" , folder = "CI-Pipeline" , repo_url = "https://github.com/priyanshuprafful/roboshop-infra"},
-    { name = "payment" , folder = "CI-Pipeline" , repo_url = "https://github.com/priyanshuprafful/roboshop-infra"},
-    { name = "shipping" , folder = "CI-Pipeline" , repo_url = "https://github.com/priyanshuprafful/roboshop-infra"},
-    { name = "user" , folder = "CI-Pipeline" , repo_url = "https://github.com/priyanshuprafful/roboshop-infra"},
-    { name = "cart" , folder = "CI-Pipeline" , repo_url = "https://github.com/priyanshuprafful/roboshop-infra"}
+    { name = "frontend" , folder = "CI-Pipeline" , repo_url = "https://github.com/priyanshuprafful/frontend"},
+    { name = "catalogue" , folder = "CI-Pipeline" , repo_url = "https://github.com/priyanshuprafful/catalogue"},
+    { name = "payment" , folder = "CI-Pipeline" , repo_url = "https://github.com/priyanshuprafful/payment"},
+    { name = "shipping" , folder = "CI-Pipeline" , repo_url = "https://github.com/priyanshuprafful/shipping"},
+    { name = "user" , folder = "CI-Pipeline" , repo_url = "https://github.com/priyanshuprafful/user"},
+    { name = "cart" , folder = "CI-Pipeline" , repo_url = "https://github.com/priyanshuprafful/cart"}
   ]
 
 }
