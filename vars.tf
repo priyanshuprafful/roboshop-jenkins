075c75a57a120b8c9bd218cd7b48fd688233ac59
priyanshuprafful <priyanshuprafful@gmail.com>
2024-06-05 04:25:50 +0530

    updating ./roboshop-jenkins

--- vars.tf
+++ vars.tf
@@ -2,9 +2,9 @@ variable "folders" {
   default = ["Infra" , "CI-Pipeline"]
 }
 
-variable "jobs" {
+variable "m-jobs" {
   default = [
-    { name = "roboshop" , folder = "Infra" , repo_url = "https://github.com/priyanshuprafful/roboshop-infra"},
+
     { name = "frontend" , folder = "CI-Pipeline" , repo_url = "https://github.com/priyanshuprafful/frontend"},
     { name = "catalogue" , folder = "CI-Pipeline" , repo_url = "https://github.com/priyanshuprafful/catalogue"},
     { name = "payment" , folder = "CI-Pipeline" , repo_url = "https://github.com/priyanshuprafful/payment"},
@@ -13,4 +13,9 @@ variable "jobs" {
     { name = "cart" , folder = "CI-Pipeline" , repo_url = "https://github.com/priyanshuprafful/cart"}
   ]
 
+}
+
+variable "s-jobs" {
+  default = [
+    { name = "roboshop" , folder = "Infra" , repo_url = "https://github.com/priyanshuprafful/roboshop-infra"}]
 }
